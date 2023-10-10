d51a31bfd2e2fb79faea39f955fd56698f826316
Alexandra Stone <alex@cmatm.com>
2023-10-10 15:28:04 -0600

    gps extraction and copy script

---
+++ ExtractAndProcessGPS.py
@@ -0,0 +1,70 @@
+import subprocess as sp
+import json
+import pprint
+import argparse
+
+import googlemaps
+from datetime import datetime
+from geopy.geocoders import GoogleV3
+
+
+#read apikey from file
+f = open("googleApiKey", "r")
+googleApiKey = f.read().strip()
+f.close()
+
+parser = argparse.ArgumentParser(
+    description='Extracts GPS subtitles from Ambarella MP4s.',
+    epilog='Written by Alexandra Stone 2023.',
+    add_help=True)
+
+parser.add_argument('filename', help='Path of the file you want to extract from.')
+
+args = parser.parse_args()
+
+videofile = args.filename
+
+geolocator = GoogleV3(api_key=googleApiKey)
+
+out = sp.run(['ffmpeg','-i',videofile, '-map', 's:0', '-f','ass','-'],\
+             stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines=True)
+subtitle = out.stdout
+subtitles = subtitle.split("Dialogue: ")
+subtitles = subtitles[1:]
+gpsArray = [None] * 0
+
+for dialogue in subtitles:
+    dialogue = dialogue.split("  ")[1].strip()
+    dialogue = dialogue.split("$G:")[1]
+    leftover = dialogue.split(" ")
+    date = leftover[0]
+    leftover = leftover[1].split("-")
+    time = leftover[0]
+    latitude = leftover[1]
+    if latitude[0] == "S":
+        latitude = "-" + latitude[1:]
+    else:
+        latitude = latitude[1:]
+    longitude = leftover[2]
+    if longitude[0] == "E":
+        longitude = "-" + longitude[1:]
+    else:
+        longitude = longitude[1:]
+    gpsData = {
+        "date": date,
+        "time": time,
+        "latitude": float(latitude),
+        "longitude": float(longitude),
+        "geoCoords": f'{latitude}, {longitude}'
+        }
+    gpsArray.append(gpsData)
+found = False
+for data in gpsArray:
+    if data['latitude'] or data['longitude']:
+        found = True
+        location = geolocator.reverse("22.5757344, 88.4048656")
+        print(location[0].address)
+        break
+
+if not found:
+    print("No Location")
---
+++ moveFiles.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# Requirements. Relies on findmnt, mountpoint, cifsutils
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mountPath=
+fileServerMount=
+serverHost=
+
+is_mounted() {
+    findmnt --source "$1" > "/dev/null"
+}
+
+unmount_auto_retry() {
+
+busy=true
+while $busy
+do
+ if mountpoint -q "$1"
+ then
+  umount "$1" 2> /dev/null
+  if [ $? -eq 0 ]
+  then
+   busy=false  # umount successful
+  else
+   echo -n 'Failed to unmount, trying again in 5 seconds.'  # output to show that the script is alive
+   sleep 5      # 5 seconds for testing, modify to 300 seconds later on
+  fi
+ else
+  busy=false  # not mounted
+ fi
+done
+}
+
+cp_p()
+{
+
+    filename=$(basename ${1})
+    strace -q -ewrite cp -- "${1}" "${2}" 2>&1 \
+        | awk '{
+        count += $NF
+                if (count % 10 == 0) {
+                    percent = count / total_size * 100
+                    left = percent * size / 100
+                    printf "%3d%%[", percent
+                    for (i=0;i<=percent;i++)
+                        printf "="
+                    printf ">"
+                    for (i=percent;i<100;i++)
+                        printf " "
+                    printf "] " left "/" size "\r"
+                }
+            }
+            END { print ""}' total_size=$(stat -c '%s' "${1}") count=0 size=$(du -h ${1} | awk '{print $1}')
+            echo "$filename copied sucessfully."
+}
+
+python enableUSBMode.py
+sleep 5 # Sleep until devices are mounted
+mkdir $mountPath
+mkdir $fileServerMount
+mount -t cifs $serverHost $fileServerMount -o cred=$SCRIPT_DIR/sambaCreds,vers=3.0
+#mount block devices sequentially
+for blk in /dev/sd*1
+do
+    if ! is_mounted $blk; then
+        usbPath=$(udevadm info -q path -n $blk | cut -d'/' -f7)
+        mount $blk $mountPath
+        if [ -d "$mountPath/DCIM" ]; then
+            #Get Officer name
+            fileCheck=(mount/DCIM/*/*)
+            name=$(basename $fileCheck | cut -d_ -f1)
+            if [ ! -d "$fileServerMount/$name" ]; then
+                mkdir $fileServerMount/$name
+            fi
+            echo "Copying files from $name's bodycam on $blk ($usbPath), please do not remove the device!"
+            for folder in $mountPath/DCIM/*
+            do
+                filename=$(basename $folder)
+                year=$(echo $filename | cut -c1-2)
+                month=$(echo $filename | cut -c3-4)
+                day=$(echo $filename | cut -c5-6)
+                echo "Organizing files."
+                if [ ! -d "$fileServerMount/$name/20$year" ]; then
+                    mkdir $fileServerMount/$name/20$year
+                fi
+                if [ ! -d "$fileServerMount/$name/20$year/$month" ]; then
+                    mkdir $fileServerMount/$name/20$year/$month
+                fi
+                if [ ! -d "$fileServerMount/$name/20$year/$month/$day" ]; then
+                    mkdir $fileServerMount/$name/20$year/$month/$day
+                fi
+                echo "Copying $month/$day/$year."
+                for video in $folder/*
+                do
+                    echo $video
+                    location=$(python ExtractAndProcessGPS.py $video)
+                    videoname=$(basename $video)
+                    time=$(echo $video | cut -c 21-26)
+                    cp_p "$video" "$fileServerMount/$name/20$year/$month/$day/$time-$location"
+                done
+            done
+        else
+            echo "No files to copy from $blk, continuing."
+        fi
+        echo "Unmounting $name's bodycam."
+        unmount_auto_retry $mountPath
+    fi
+done
+unmount_auto_retry $fileServerMount
+rmdir $mountPath
+rmdir $fileServerMount
